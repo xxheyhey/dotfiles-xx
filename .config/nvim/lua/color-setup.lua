a46a255e113fed255dd0a2ffadf2bf68f3e4d62a
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-01-05 12:46:19 +0100

    further nvim config, check keybinds

--- .config/nvim/lua/color-setup.lua
+++ .config/nvim/lua/color-setup.lua
@@ -1,13 +1,19 @@
 -- theme customization
-require('onedark').setup {
-  transparent = true,
-  code_style = { comments = 'none' },
-  lualine = { transparent = true },
-  diagnostics = { background = false },
+-- require('onedark').setup {
+--   transparent = true,
+--   code_style = { comments = 'none' },
+--   lualine = { transparent = true },
+--   diagnostics = { background = false },
+-- }
+
+require('rose-pine').setup {
+	disable_background = true,
+  disable_float_background = true,
+	disable_italics = true
 }
 
 function Color(color)
-	color = color or "onedark"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
