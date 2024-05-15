8a9c7256be4e6f85f6c20f10444217fb19101f47
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-05-15 16:39:20 +0200

    Update oil-setup.lua

--- .config/nvim/lua/oil-setup.lua
+++ .config/nvim/lua/oil-setup.lua
@@ -21,55 +21,14 @@ require("oil").setup({
   -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
   -- (:help prompt_save_on_select_new_entry)
   prompt_save_on_select_new_entry = true,
-  -- Oil will automatically delete hidden buffers after this delay
-  -- You can set the delay to false to disable cleanup entirely
-  -- Note that the cleanup process only starts when none of the oil buffers are currently displayed
-  cleanup_delay_ms = 2000,
-  -- Constrain the cursor to the editable parts of the oil buffer
-  -- Set to `false` to disable, or "name" to keep it on the file names
-  constrain_cursor = "editable",
-  -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
-  -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
-  -- Additionally, if it is a string that matches "actions.<name>",
-  -- it will use the mapping at require("oil.actions").<name>
-  -- Set to `false` to remove a keymap
   -- See :help oil-actions for a list of all available actions
   keymaps = {
-    ["g?"] = "actions.show_help",
-    ["<CR>"] = "actions.select",
-    ["<C-s>"] = "actions.select_vsplit",
     ["<C-h>"] = false,
-    ["<C-t>"] = "actions.select_tab",
-    ["<C-p>"] = "actions.preview",
-    ["<C-c>"] = "actions.close",
     ["<C-l>"] = false,
-    ["-"] = "actions.parent",
-    ["_"] = "actions.open_cwd",
-    ["`"] = "actions.cd",
-    ["~"] = "actions.tcd",
-    ["gs"] = "actions.change_sort",
-    ["gx"] = "actions.open_external",
-    ["g."] = "actions.toggle_hidden",
-    ["g\\"] = "actions.toggle_trash",
   },
-  -- Set to false to disable all of the above keymaps
-  use_default_keymaps = true,
   view_options = {
-    -- Show files and directories that start with "."
     show_hidden = true,
   },
-  -- Configuration for the floating window in oil.open_float
-  float = {
-    -- Padding around the floating window
-    padding = 5,
-    max_width = 48,
-    max_height = 18,
-    border = "rounded",
-    win_options = {
-      winblend = 0,
-    },
-    -- This is the config that will be passed to nvim_open_win.
-    -- Change values here to customize the layout
     override = function(conf)
       return conf
     end,
