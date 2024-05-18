a46623f8bd7751b9c2c0c19f966ed87bd51be310
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-05-18 21:38:54 +0200

    nvim options

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -1,94 +1,71 @@
--- [[ Setting options ]]
 -- See `:help vim.opt`
 
--- Set highlight on search
-vim.opt.hlsearch = false
+local opt = vim.opt
 
--- Make line numbers default
-vim.wo.number = true
-vim.opt.nu = true
+opt.inccommand = "split"
 
--- Relative line numbers
-vim.opt.relativenumber = true
+-- Set highlight off on search
+opt.hlsearch = false
+
+-- Make relative line numbers default
+opt.number = true
+opt.relativenumber = true
 
 -- Enable mouse mode
-vim.opt.mouse = 'a'
-
--- Save undo history
-vim.opt.undofile = true
+opt.mouse = "a"
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+opt.signcolumn = "yes"
 
 -- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 500
-
--- Set completeopt to have a better completion experience
-vim.opt.completeopt = 'menuone,noselect'
-
-vim.opt.termguicolors = true
+opt.updatetime = 250
+opt.timeoutlen = 500
 
 -- Tab width
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
 -- Indentation
-vim.opt.smartindent = true
-vim.opt.autoindent = true
+opt.smartindent = true
+opt.autoindent = true
 
 -- Disable line wrap
-vim.opt.wrap = false
+opt.wrap = false
 
 -- Enable break indent for wrap mode
-vim.o.breakindent = true
+opt.breakindent = true
 
 -- Undo history settings
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
 -- Formatting options
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
-
--- Taken from tjdevries's github, but NOTE: does not work
-vim.opt.formatoptions = vim.opt.formatoptions
-    - "a"                  -- Auto formatting is BAD.
-    - "t"                  -- Don't auto format my code. I got linters for that.
-    - "c"                  -- In general, I like it when comments respect textwidth
-    - "o"                  -- O and o, don't continue comments
-    - "r"                  -- But do continue when pressing enter.
-    + "n"                  -- Indent past the formatlistpat, not underneath it.
-    - "2"                  -- Idk what this does
-
-vim.opt.joinspaces = false -- Same for this one...
-
+opt.formatoptions:remove("o")
 
 -- Other settings
-vim.opt.incsearch = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-vim.opt.colorcolumn = "80"
-vim.diagnostic.config({
-    virtual_text = true
-})
-vim.opt.shortmess:append({ I = true }) -- Remove intro screen because of flicker bug
+opt.termguicolors = true
+opt.incsearch = true
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+opt.colorcolumn = "80"
+vim.diagnostic.config({ virtual_text = true })
 
 -- Highlight on yank
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
--- .config/nvim/plugin/remaps.lua
+++ .config/nvim/plugin/remaps.lua
@@ -1,114 +1,121 @@
--- Keymaps
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<CR>', '<Nop>', { silent = true })
+-- See `:help set()`
+local set = vim.keymap.set
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+set({ 'n', 'v' }, '<CR>', '<Nop>', { silent = true })
+
+set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Open the Oil file explorer
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Move lines up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
 
 -- Paste and delete without changing paste buffer
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without changing paste buffer" })
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without changing paste buffer" })
+set("x", "<leader>p", [["_dP]], { desc = "Paste without changing paste buffer" })
+set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without changing paste buffer" })
 
 -- yank to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
+set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
+set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
 
 -- Make file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "'chmod + x' on current file", silent = true })
+set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "'chmod + x' on current file", silent = true })
 
 -- tmux-sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Launch tmux-sessionizer script" })
-vim.keymap.set("n", "<C-g>", "<cmd>silent !open-current-dir<CR>", { desc = "Launch open-current-dir script" })
-vim.keymap.set("n", "<leader>gh", "<cmd>silent !open-home-dir<CR>", { desc = "Launch open-current-dir script" })
+set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Launch tmux-sessionizer script" })
+set("n", "<C-g>", "<cmd>silent !open-current-dir<CR>", { desc = "Launch open-current-dir script" })
+set("n", "<leader>gh", "<cmd>silent !open-home-dir<CR>", { desc = "Launch open-current-dir script" })
 
 -- Format file
-vim.keymap.set("n", "<leader>f", function() require("conform").format { lsp_fallback = true, quiet = true, } end,
+set("n", "<leader>f", function() require("conform").format { lsp_fallback = true, quiet = true, } end,
     { desc = "Format file" })
 
 -- Quickfix list
-vim.keymap.set("n", "<up>", "<cmd>cnext<CR>zz", { desc = "Quickfix list next" })
-vim.keymap.set("n", "<down>", "<cmd>cprev<CR>zz", { desc = "Quickfix list previous" })
-vim.keymap.set("n", "<leader><up>", "<cmd>lnext<CR>zz", { desc = "Idk" })
-vim.keymap.set("n", "<leader><down>", "<cmd>lprev<CR>zz", { desc = "Idk" })
+set("n", "<up>", "<cmd>cnext<CR>zz", { desc = "Quickfix list next" })
+set("n", "<down>", "<cmd>cprev<CR>zz", { desc = "Quickfix list previous" })
+set("n", "<leader><up>", "<cmd>lnext<CR>zz", { desc = "Idk" })
+set("n", "<leader><down>", "<cmd>lprev<CR>zz", { desc = "Idk" })
 
 -- Switch to next window
-vim.keymap.set("n", "<tab>", "<cmd>winc w<CR>", { desc = "Change focus to next vim window", silent = true })
+set("n", "<tab>", "<cmd>winc w<CR>", { desc = "Change focus to next vim window", silent = true })
 
 -- Toggle wrap
-vim.keymap.set("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle wrap", silent = true })
+set("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle wrap", silent = true })
 
 -- Toggle the color scheme
-vim.keymap.set('n', '<leader>th', '<cmd>lua ToggleColorScheme()<CR>', { desc = "Toggle theme", silent = true })
+set('n', '<leader>th', '<cmd>lua ToggleColorScheme()<CR>', { desc = "Toggle theme", silent = true })
 
 -- Toggle transparency (custom scripts)
-vim.keymap.set("n", "<leader>tro", "<cmd>silent !transon<CR>", { desc = "Transparency on" })
-vim.keymap.set("n", "<leader>trf", "<cmd>silent !transoff<CR>", { desc = "Transparency off" })
+set("n", "<leader>tro", "<cmd>silent !transon<CR>", { desc = "Transparency on" })
+set("n", "<leader>trf", "<cmd>silent !transoff<CR>", { desc = "Transparency off" })
 
 -- Toggle ZenMode
-vim.keymap.set("n", "<F11>", "<cmd>ZenMode<CR>", { desc = "Start ZenMode", silent = true })
+set("n", "<F11>", "<cmd>ZenMode<CR>", { desc = "Start ZenMode", silent = true })
 
 -- Open current file in browser
-vim.keymap.set("n", "gX", "<cmd>silent !google-chrome-stable % &<CR>", { desc = "Open current file in Chrome" })
-vim.keymap.set("n", "<leader>oc", "<cmd>silent !google-chrome-stable % &<CR>", { desc = "Open current file in Chrome" })
-vim.keymap.set("n", "<leader>of", "<cmd>silent !firefox % &<CR>", { desc = "Open current file in Firefox" })
+set("n", "gX", "<cmd>silent !google-chrome-stable % &<CR>", { desc = "Open current file in Chrome" })
+set("n", "<leader>oc", "<cmd>silent !google-chrome-stable % &<CR>", { desc = "Open current file in Chrome" })
+set("n", "<leader>of", "<cmd>silent !firefox % &<CR>", { desc = "Open current file in Firefox" })
 
 -- nvim-spectre
-vim.keymap.set('n', '<leader>Sr', '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
-vim.keymap.set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+set('n', '<leader>Sr', '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
+set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
     { desc = "Search current word" })
-vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
-vim.keymap.set('n', '<leader>Sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
+set('n', '<leader>Sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
     { desc = "Search on current file" })
 
 -- Simpler search and replace than nvim-spectre:
 -- Search and replace in regex mode, '\(' means match bracket and not open atom
-vim.keymap.set("n", "<leader>sr", ":%s/\\v", { desc = "Search and replace" })
+set("n", "<leader>sr", ":%s/\\v", { desc = "Search and replace" })
 -- Search and replace current word
-vim.keymap.set("n", "<leader>sw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+set("n", "<leader>sw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
     { desc = "Change the current and all the same words" })
 
 -- Neotree
-vim.keymap.set('n', '<leader>T', '<cmd>Neotree reveal<CR>', { desc = "Neotree", silent = true })
+set('n', '<leader>T', '<cmd>Neotree reveal<CR>', { desc = "Neotree", silent = true })
 
 -- Diffview
-vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "DiffviewOpen", silent = true })
-vim.keymap.set("n", "<leader>gq", "<cmd>DiffviewClose<CR>", { desc = "DiffviewClose", silent = true })
+set("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "DiffviewOpen", silent = true })
+set("n", "<leader>gq", "<cmd>DiffviewClose<CR>", { desc = "DiffviewClose", silent = true })
 
 -- Search through all keymaps with Telescope
-vim.keymap.set("n", "<leader>key", "<cmd>Telescope keymaps<CR>", { desc = "Search through keymaps", silent = true })
+set("n", "<leader>key", "<cmd>Telescope keymaps<CR>", { desc = "Search through keymaps", silent = true })
 
 -- Neogit
-vim.keymap.set("n", "<leader>gs", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
-vim.keymap.set("n", "<leader>gc", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
+set("n", "<leader>gs", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
+set("n", "<leader>gc", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
 
 -- Biscuits toggle
-vim.keymap.set("n", "<leader>tb", function() require("nvim-biscuits").toggle_biscuits() end, { desc = "Toggle Biscuits" })
+set("n", "<leader>tb", function() require("nvim-biscuits").toggle_biscuits() end, { desc = "Toggle Biscuits" })
 
 -- Comment
-vim.keymap.set("n", "<leader>c", "<Nop>", { desc = "Insert comment" })
+set("n", "<leader>c", "<Nop>", { desc = "Insert comment" })
 
 -- Undotree
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
+set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
 
 -- Other
-vim.keymap.set("n", "J", "mzJ`z")
+set("n", "J", "mzJ`z")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "Q", "<nop>")
+-- There are builtin keymaps for this now, but I (tjdevries) like that it shows
+-- the float when I navigate to the error - so I override them.
+set("n", "]d", vim.diagnostic.goto_next)
+set("n", "[d", vim.diagnostic.goto_prev)
+
+
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+set("i", "<C-c>", "<Esc>")
+set("n", "Q", "<nop>")
 
 -- Functions
 vim.cmd [[command! -nargs=0 W :w]]
