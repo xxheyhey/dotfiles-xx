65eedb02bd32fbf6f07a50674eececf12b5e8947
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-05-22 16:50:08 +0200

    small tweaks

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,14 +1,14 @@
 -- See `:help set()`
 local set = vim.keymap.set
 
-set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-set({ 'n', 'v' }, '<CR>', '<Nop>', { silent = true })
+set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+set({ "n", "v" }, "<CR>", "<Nop>", { silent = true })
 
 -- Diagnostics
-set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Open the Oil file explorer
 set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
@@ -24,6 +24,7 @@ set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without changing paste 
 -- yank to clipboard
 set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
 set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
+set("v", "<leader>Y", [["+y]], { desc = "Yank to clipboard" })
 
 -- Make file executable
 set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "'chmod + x' on current file", silent = true })
@@ -34,8 +35,9 @@ set("n", "<C-g>", "<cmd>silent !open-current-dir<CR>", { desc = "Launch open-cur
 set("n", "<leader>gh", "<cmd>silent !open-home-dir<CR>", { desc = "Launch open-current-dir script" })
 
 -- Format file
-set("n", "<leader>f", function() require("conform").format { lsp_fallback = true, quiet = true, } end,
-    { desc = "Format file" })
+set("n", "<leader>f", function()
+	require("conform").format({ lsp_fallback = true, quiet = true })
+end, { desc = "Format file" })
 
 -- Quickfix list
 set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Quickfix list next" })
@@ -48,7 +50,7 @@ set("n", "<tab>", "<cmd>winc w<CR>", { desc = "Change focus to next vim window",
 set("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle wrap", silent = true })
 
 -- Toggle the color scheme
-set('n', '<leader>th', '<cmd>lua ToggleColorScheme()<CR>', { desc = "Toggle theme", silent = true })
+set("n", "<leader>th", "<cmd>lua ToggleColorScheme()<CR>", { desc = "Toggle theme", silent = true })
 
 -- Toggle transparency (custom scripts)
 set("n", "<leader>tro", "<cmd>silent !transon<CR>", { desc = "Transparency on" })
@@ -63,19 +65,31 @@ set("n", "<leader>oc", "<cmd>silent !google-chrome-stable % &<CR>", { desc = "Op
 set("n", "<leader>of", "<cmd>silent !firefox % &<CR>", { desc = "Open current file in Firefox" })
 
 -- nvim-spectre
-set('n', '<leader>Sr', '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
-set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
-    { desc = "Search current word" })
-set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
-set('n', '<leader>Sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
-    { desc = "Search on current file" })
+set("n", "<leader>Sr", '<cmd>lua require("spectre").toggle()<CR>', { desc = "Toggle Spectre" })
+set(
+	"n",
+	"<leader>Sw",
+	'<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+	{ desc = "Search current word" }
+)
+set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', { desc = "Search current word" })
+set(
+	"n",
+	"<leader>Sf",
+	'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+	{ desc = "Search on current file" }
+)
 
 -- Simpler search and replace than nvim-spectre:
 -- Search and replace in regex mode, '\(' means match bracket and not open atom
 set("n", "<leader>sr", ":%s/\\v", { desc = "Search and replace" })
 -- Search and replace current word
-set("n", "<leader>sw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Change the current and all the same words" })
+set(
+	"n",
+	"<leader>sw",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Change the current and all the same words" }
+)
 
 -- Diffview
 set("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { desc = "DiffviewOpen", silent = true })
@@ -85,14 +99,17 @@ set("n", "<leader>gq", "<cmd>DiffviewClose<CR>", { desc = "DiffviewClose", silen
 set("n", "<leader>key", "<cmd>Telescope keymaps<CR>", { desc = "Search through keymaps", silent = true })
 
 -- Neogit
-set("n", "<leader>gs", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
-set("n", "<leader>gc", function() require('neogit').open({ kind = 'split' }) end, { desc = "Neogit" })
+set("n", "<leader>gs", function()
+	require("neogit").open({ kind = "split" })
+end, { desc = "Neogit" })
+set("n", "<leader>gc", function()
+	require("neogit").open({ kind = "split" })
+end, { desc = "Neogit" })
 
 -- Biscuits toggle
-set("n", "<leader>tb", function() require("nvim-biscuits").toggle_biscuits() end, { desc = "Toggle Biscuits" })
-
--- Comment
-set("n", "<leader>c", "<Nop>", { desc = "Insert comment" })
+set("n", "<leader>tb", function()
+	require("nvim-biscuits").toggle_biscuits()
+end, { desc = "Toggle Biscuits" })
 
 -- Undotree
 set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
@@ -108,7 +125,7 @@ set("i", "<C-c>", "<Esc>")
 set("n", "Q", "<nop>")
 
 -- Functions
-vim.cmd [[command! -nargs=0 W :w]]
-vim.cmd [[command! -nargs=0 WQ :wq]]
-vim.cmd [[command! -nargs=0 Wq :wq]]
-vim.cmd [[command! -nargs=0 Q :q]]
+vim.cmd([[command! -nargs=0 W :w]])
+vim.cmd([[command! -nargs=0 WQ :wq]])
+vim.cmd([[command! -nargs=0 Wq :wq]])
+vim.cmd([[command! -nargs=0 Q :q]])
