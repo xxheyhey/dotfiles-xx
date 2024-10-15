fd8116a4315e77008d6372f2c73ea98af485f72d
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-10-15 23:19:23 +0200

    tried to fix the lualine bug in newest nvim update

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -7,18 +7,9 @@ return {
 		config = function()
 			require("lualine").setup({
 				options = {
-					icons_enabled = true,
-					theme = Mycolor,
+					theme = "auto",
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
-					ignore_focus = {},
-					always_divide_middle = true,
-					globalstatus = false,
-					refresh = {
-						statusline = 1000,
-						tabline = 1000,
-						winbar = 1000,
-					},
 				},
 				sections = {
 					lualine_a = {
@@ -80,17 +71,6 @@ return {
 						},
 					},
 				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { "filename" },
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-				tabline = {},
-				winbar = {},
-				inactive_winbar = {},
 			})
 		end,
 	},
